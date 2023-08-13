7283579385705c96c36bfb22a250edaa9321e55b
bankyatta <siribank2312@gmail.com>
2023-08-13 21:57:05 +0700

    Finish exercises 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(
+    (sum, current) => sum + current
+  , 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(
+    (sum, current) => sum * current
+  )
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return 1;
+  }
+
+  let sum = 1;
+  for (let i = 1; i <= number; i++) {
+    sum *= i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
